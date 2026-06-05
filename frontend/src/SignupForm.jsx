33b542d08a47ba3888283e9dd000470837eeb254
Jemson <jemsonmislani@gmail.com>
2026-06-05 16:07:44 +0800

    create usestate for username and password, validate them

--- frontend/src/SignupForm.jsx
+++ frontend/src/SignupForm.jsx
@@ -3,22 +3,37 @@ import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 export default function SignupForm() {
+  const [user, setUser] = useState([])
+  const [username, setUserName] = useState('')
+  const [password, setUserPass] = useState('')
+
+  const handleSignupBtn = (e) => {
+    e.preventDefault()
+    if(!username || !password){
+      alert('Please enter new account')
+      return
+    }
+  }
 
   return (
     <>
-      <form>
-                <div className="flex justify-center items-center h-200">
+      <form onSubmit={handleSignupBtn}>
+            <div className="flex justify-center items-center h-200">
             <div className="ring shadow-xl ring-gray-900/5 flex-col w-110 h-120 rounded">
                 <div className="flex justify-center items-center align-middle text-4xl font-semibold text-black h-30">Signup</div>
                 <div className="flex flex-col justify-center items-center gap-5 not-odd:h-70">
                 <input 
                   className="focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 text-gray-700 py-3 px-2 w-70 border border-gray-400 rounded"
                   type="text" 
-                  placeholder="Enter email or username"/>
+                  placeholder="Enter email or username"
+                  value={username}
+                  onChange={(e) => setUserName(e.target.value)}/>
                 <input
                   className="focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 text-gray-700 py-3 px-2 w-70 border border-gray-400 rounded" 
                   type="password" 
-                  placeholder="Enter password"/>
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setUserPass(e.target.value)}/>
                 <div className='flex w-70 items-center gap-1'>
                 </div>
                   <button className="active:bg-sky-500 bg-sky-500 hover:bg-sky-700 text-white py-3 px-20 rounded text-lg cursor-pointer">Signup</button>
