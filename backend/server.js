cceef0093a5d03bc892a2005101131776a12ec55
Jemson <jemsonmislani@gmail.com>
2026-06-05 13:23:25 +0800

    create an api endpoint for creating a login, validate inside.

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,38 @@ app.post('/createUsers', async (req, res) => {
     }
 })
 
+// create login
+app.post('/createLogin', async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+        const result = await pool.query('SELECT * FROM users WHERE email=$1', [
+            email
+        ])
+
+        if(result.rows.length === 0){
+            return res.status(400).send({message: 'User not found'})
+        }
+
+        const user = result.rows[0]
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch) {
+            return res.status(400).json({message: 'Invalid credentials.'})
+        }
+
+        const token = jwt.sign({id: user.id}, 'YOUR_JWT_SECRET', {expiresIn: '1h'})
+        return res.json({
+            token,
+            user: {id: user.id, email: user.email}
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Server error')
+    }
+})
+
 const PORT = 3009;
 app.listen(PORT, () => {
     console.log(`Jem! Your server is running on Port ${PORT}`)
