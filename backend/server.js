1d0221d83768fe0493463ff2c32761ea7f80f9c3
Jemson <jemsonmislani@gmail.com>
2026-06-05 12:02:55 +0800

    set up database, create an api endpoint for adding or creating a user. Use postman to check, success, 200 OK.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const cors = require('cors')
 const { Pool } = require('pg')
-const jsonwebtoken = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+const pool = new Pool({
+    user: 'postgres',
+    password: 'Im_Jem23*',
+    host: 'localhost',
+    database: 'jwt_oauth_db',
+    port: 5432
+})
+
+// create users
+app.post('/createUsers', async (req, res) => {
+
+    try {
+        const { email, password} = req.body
+        const hashedPw = await bcrypt.hash(password, 10)
+
+        const result = await pool.query('INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *', [
+            email, hashedPw
+        ])
+        res.json(result.rows[0])
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Server error')
+    }
+})
 
 const PORT = 3009;
 app.listen(PORT, () => {
