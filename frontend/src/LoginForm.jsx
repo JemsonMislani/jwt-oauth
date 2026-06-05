b81181d2b051546a0deeb2f0589b38184f5ab0ae
Jemson <jemsonmislani@gmail.com>
2026-06-05 11:10:59 +0800

    Install dependencies, do routing.

---
+++ frontend/src/LoginForm.jsx
@@ -0,0 +1,10 @@
+
+export default function LoginForm() {
+
+  return (
+    <>
+        <h1>Login Form</h1>
+    </>
+  )
+}
+
---
+++ frontend/src/SignupForm.jsx
@@ -0,0 +1,10 @@
+
+export default function SignupForm() {
+
+  return (
+    <>
+        <h1>Signup Form</h1>
+    </>
+  )
+}
+
