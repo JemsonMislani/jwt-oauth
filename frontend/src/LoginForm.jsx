4d9e16ad4a6a2afa34ea8cad36b78e72cee621b8
Jemson <jemsonmislani@gmail.com>
2026-06-05 14:26:19 +0800

    style frontend login form using tailwind.

--- frontend/src/LoginForm.jsx
+++ frontend/src/LoginForm.jsx
@@ -1,9 +1,37 @@
-
+import { Link } from 'react-router-dom'
 export default function LoginForm() {
 
   return (
     <>
-        <h1>Login Form</h1>
+        <div className="flex justify-center items-center h-200">
+            <div className="ring shadow-xl ring-gray-900/5 flex-col w-110 h-120 rounded">
+                <div className="flex justify-center items-center align-middle text-4xl font-semibold text-black h-30">Login</div>
+                <div className="flex flex-col justify-center items-center gap-5 not-odd:h-70">
+                <input 
+                  className="focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 text-gray-700 py-3 px-2 w-70 border border-gray-400 rounded"
+                  type="text" 
+                  placeholder="Enter email or username"/>
+                <input
+                  className="focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 text-gray-700 py-3 px-2 w-70 border border-gray-400 rounded" 
+                  type="text" 
+                  placeholder="Enter password"/>
+                <div className='flex w-70 items-center gap-1'>
+                  <input
+                  type="checkbox"
+                  id="remember"
+                  className="h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300 rounded"
+                />
+                <label htmlFor="">Remember me</label>
+                </div>
+                  <button className="active:bg-sky-500 bg-sky-500 hover:bg-sky-700 text-white py-3 px-20 rounded text-lg cursor-pointer">Login</button>
+                  <div>
+                    <Link 
+                      className='text-sky-500 hover:text-sky-700 active:text-sky-500'
+                      to='/create'>No account yet?</Link>
+                  </div>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
